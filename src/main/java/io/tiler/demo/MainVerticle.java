59d6b51c2bed206092512fddff82cd1c9a4f52d9
Simon Dean <simon@simondean.org>
2015-06-27 18:33:09 +0100

    Load redis config from REDISTOGO_URL environment variable

--- src/main/java/io/tiler/demo/MainVerticle.java
+++ src/main/java/io/tiler/demo/MainVerticle.java
@@ -3,15 +3,22 @@ package io.tiler.demo;
 import org.simondean.vertx.async.Async;
 import org.simondean.vertx.async.AsyncResultHandlerWrapper;
 import org.vertx.java.core.Future;
+import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
 public class MainVerticle extends Verticle {
   private Logger logger;
+  private Map<String, String> env;
 
   public void start(final Future<Void> startedResult) {
     logger = container.logger();
+    env = container.env();
     JsonObject config = container.config();
 
     Async.series()
@@ -26,5 +33,49 @@ public class MainVerticle extends Verticle {
         logger.info("MainVerticle started");
         startedResult.setResult(null);
       });
+  }
+
+  private void mergeConfigWithEnvironmentVariables(JsonObject config) {
+    mergeConfigWithRedisEnvironmentVariables(config);
+  }
+
+  private void mergeConfigWithRedisEnvironmentVariables(JsonObject config) {
+    JsonObject server = config.getObject("server");
+
+    if (server == null) {
+      logger.warn("'server' is missing from the config");
+      return;
+    }
+
+    JsonObject redis = server.getObject("redis");
+
+    if (redis == null) {
+      logger.warn("'redis' is missing from the config");
+      return;
+    }
+
+    String redisUrlString = getEnvironmentVariable("REDISTOGO_URL");
+
+    if (redisUrlString != null) {
+      logger.info("Using Redis To Go");
+      
+      try {
+        URL redisURL = new URL(redisUrlString);
+        redis.putString("host", redisURL.getHost());
+        redis.putNumber("port", redisURL.getPort());
+      } catch (MalformedURLException e) {
+        logger.error("Error parsing Redis URL", e);
+      }
+    }
+  }
+
+  private String getEnvironmentVariable(String name) {
+    String value = env.get(name);
+
+    if (value == null) {
+      logger.warn("Environment variable '" + name + "' has not been set");
+    }
+
+    return value;
   }
 }
